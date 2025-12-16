3c3ddc230c64a8d8d46a1a72a4ecd84396279a74
priyank2898-git <priyanks2898@gmail.com>
2025-12-16 19:26:31 +0530

    First commit before initializing DVC

---
+++ DVC/mycode.py
@@ -0,0 +1,23 @@
+import os 
+import pandas as pd 
+
+data={
+    "Name":["Alice", "Bob","Charlie"],
+    "Age":[21,23,45],
+    "City":["New York","Chicago","San Francisco"]
+}
+
+df=pd.DataFrame(data)
+
+
+
+
+#file path
+direc_path='data'
+os.makedirs(direc_path,exist_ok=True)
+
+file_path=os.path.join(direc_path,'sample_data.csv')
+
+df.to_csv(file_path,index=False)
+
+print(f"CSV file saved to path {file_path}")
